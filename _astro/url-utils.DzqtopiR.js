b1babeaaf0f589a40185bf1de7fa8aaec968bd3c
1992724048 <1992724048@qq.com>
2026-08-13 16:59:58 +0000

    Deploying to pages from @ 1992724048/blog@de7dc6117adaf6fd30fedb2d4f5ec10a7531d6ac 🚀

---
+++ _astro/url-utils.DzqtopiR.js
@@ -0,0 +1,1 @@
+import"./translation.ClZYkzKw.js";import"./config.B5ngtNM-.js";import"./date-utils.BBwtfbs4.js";function i(r,e){return r.replace(/^\/|\/$/g,"").toLowerCase()===e.replace(/^\/|\/$/g,"").toLowerCase()}function t(...r){return r.join("/").replace(/\/+/g,"/")}function p(r){return t("","/",r)}export{p as n,i as t};
